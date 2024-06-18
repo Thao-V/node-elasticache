ba2353785d7f309eaa55b94e9dfd05cce68a04e4
thao.vu <thaovu@miu.edu>
2024-06-18 13:37:22 -0500

    added code

---
+++ app.js
@@ -0,0 +1,37 @@
+const redis = require('redis');
+require('dotenv').config();
+
+// Replace with your ElastiCache endpoint and port
+const redisHost = process.env.REDIS_HOST;
+const redisPort = process.env.REDIS_PORT;
+
+// Create a Redis client
+const client = redis.createClient({
+  host: redisHost,
+  port: redisPort,
+});
+
+async function connectToRedis(){
+  try {
+    // Connect to Redis
+    await client.connect();
+
+    // Set a value for a key
+    const key = 'myKey';
+    const value = 'Hello from Node.js!';
+    await client.set(key, value);
+    console.log(`Successfully set key: ${key} with value: ${value}`);
+
+    // Retrieve the value
+    const retrievedValue = await client.get(key);
+    console.log(`Retrieved value for key: ${key} is: ${retrievedValue}`);
+
+  } catch (error) {
+    console.error(error);
+  } finally {
+    // Disconnect from Redis
+    await client.quit();
+  }
+};
+
+connectToRedis();
